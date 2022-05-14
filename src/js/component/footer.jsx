5a994101df179c81bdf6bb3744f1e95052a48cae
alonsogomezo <luisalonsogo95@gmail.com>
2022-05-14 03:04:31 +0000

    landing

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+//retorna jsx
+const Footer = () => {
+	let estilo = {
+		width: "100%",
+	};
+	return (
+		<div className="bg-dark text-white text-center p-5 mt-4" style={estilo}>
+			footer
+		</div>
+	);
+};
+
+export default Footer;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,23 +2,34 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import Card from "./card.jsx";
+import Footer from "./footer.jsx";
+import Jumbotron from "./jumbotron.jsx";
+import Navbar from "./navbar.jsx";
 
 //create your first component
 const Home = () => {
 	return (
 		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<Navbar />
+			<div className="container">
+				<Jumbotron />
+				<div className="my-5 d-flex flex-column flex-sm-row">
+					<div className="col">
+						<Card />
+					</div>
+					<div className="col">
+						<Card />
+					</div>
+					<div className="col">
+						<Card />
+					</div>
+					<div className="col">
+						<Card />
+					</div>
+				</div>
+			</div>
+			<Footer />
 		</div>
 	);
 };
